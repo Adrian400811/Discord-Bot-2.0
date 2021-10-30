98248d8697b1fb390247bb3d70cfc267dbc134f4
Adrian40 <adrianleeho@gmail.com>
2021-10-30 18:56:30 -0400

    detailed error messages and message on join
    Added more details on error messages.
    Added message on bot joins a server.

--- bot.py
+++ bot.py
@@ -8,14 +8,19 @@ import tasks
 
 with open('./setting.json',mode='r',encoding='utf8') as jfile:
     jdata = json.load(jfile)
+    
+with open('./guild.json',mode='r+',encoding='utf8') as gfile:
+    gdata = json.load(gfile)
+    
 
-bot = commands.Bot(command_prefix='na', help_command = None)
+
+bot = commands.Bot(command_prefix='2', help_command = None)
 
 async def status_task():
     while True:
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game("</> with :heart: by @AdrianL24228638"))
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("</> with ❤ by @AdrianL24228638"))
         await asyncio.sleep(10)
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game("0help for help!"))
+        await bot.change_presence(status=discord.Status.online, activity=discord.Game("2help for help!"))
         await asyncio.sleep(10)
 
 @bot.event
@@ -26,6 +31,18 @@ async def on_ready():
     print(f"[INFO] System Time: {time}")
     print(f"[INFO] Logged in as {bot.user}")
     print(">>Bot is online<<")
+
+@bot.event
+async def on_guild_join(guild):
+    for channel in guild.text_channels:
+        if "general" in channel.name and channel.permissions_for(guild.me).send_messages:
+            await channel.send('Hey there! Thank you for using Tairitsu 2.0.\nMy source code is on Github Adrian400811/Discord-Bot-2.0.')
+            entry = {f"{guild.id}": {"name": f"{guild.name}", "mainChannel": f"{channel}"}}
+        else:
+            entry = {f"{guild.id}": {"name": f"{guild.name}", "mainChannel": None}}
+    gdata.append(entry)
+    gfile.seek(0)
+    json.dump(gdata, gfile)
 
 @bot.command(hidden=True)
 @commands.is_owner()
--- cmds/err.py
+++ cmds/err.py
@@ -4,13 +4,53 @@ from discord.ext.commands import CommandError, has_permissions, MissingPermissio
 from core.classes import Cog_Extension
 
 class Err(Cog_Extension): 
-	@commands.Cog.listener()
-	async def on_command_error(self, ctx, error):
-        
-		if isinstance(error, commands.CommandError):    
-			print(f"{error}")
-			await ctx.send(f'{error}')
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        if isinstance(error, commands.CommandError):
+            if isinstance(error, commands.CommandNotFound):
+                embed=discord.Embed(title="Command not found", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=CommandNotFound")
+                await ctx.send(embed=embed)
+                print(f"{error}")
+            elif isinstance(error, commands.MissingRequiredArgument):
+                embed=discord.Embed(title="Missing arguement", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=MissingRequiredArgument")
+                await ctx.send(embed=embed)
+                print(f"{error}")
+            elif isinstance(error, commands.MissingPermissions):
+                embed=discord.Embed(title="Missing permissions", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=MissingPermissions")
+                await ctx.send(embed=embed)
+            elif isinstance(error, commands.BotMissingPermissions):
+                embed=discord.Embed(title="Bot Missing permissions", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=BotMissingPermissions")
+                await ctx.send(embed=embed)
+            elif isinstance(error, commands.ExtensionError):
+                embed=discord.Embed(title="Extension error", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=ExtensionError")
+                await ctx.send(embed=embed)
+            elif isinstance(error, commands.ExtensionAlreadyLoaded):
+                embed=discord.Embed(title="Extension already loaded", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=ExtensionAlreadyLoaded")
+                await ctx.send(embed=embed)
+            elif isinstance(error, commands.ExtensionFailed):
+                embed=discord.Embed(title="Extension failed", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=ExtensionFailed")
+                await ctx.send(embed=embed)
+            elif isinstance(error, commands.ExtensionNotFound):
+                embed=discord.Embed(title="Extension not found", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=ExtensionNotFound")
+                await ctx.send(embed=embed)
+            elif isinstance(error, commands.ExtensionNotLoaded):
+                embed=discord.Embed(title="Extension not loaded", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=ExtensionNotLoaded")
+                await ctx.send(embed=embed)
+            elif isinstance(error, commands.NotOwner):
+                embed=discord.Embed(title="Not Owner", description=f"{error}", color=0xff0000)
+                embed.set_footer(text="err=NotOwner")
+                await ctx.send(embed=embed)
+            print(f"{error}")
 
 
 def setup(bot):
-	bot.add_cog(Err(bot))
+    bot.add_cog(Err(bot))
